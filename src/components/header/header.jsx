dc971b461e21e7a53a548ac848d3d82385034bd2
sameer-bagde <bagdesameer92@gmail.com>
2025-04-09 00:23:34 +0530

    new design

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -13,26 +13,29 @@ const Header = () => {
 
   return (
     <div className="body max-width">
-      <header className="header">
-        <div className="logo-container">
-          <Link to="/">
-            <img src={Logo} className="logo" alt="Jan Samar Foundation Logo" />
-          </Link>
-        </div>
+<header className="header">
+  <div className="navbar">
+    <div className="logo-container">
+      <Link to="/">
+        <img src={Logo} className="logo" alt="Jan Samar Foundation Logo" />
+      </Link>
+    </div>
 
-        <div className="menu-icon" onClick={toggleMenu}>
-          {isMenuOpen ? <X size={32} /> : <Menu size={32} />}
-        </div>
+    <div className="menu-icon" onClick={toggleMenu}>
+      {isMenuOpen ? <X size={32} /> : <Menu size={32} />}
+    </div>
 
-        <nav className={`nav-links ${isMenuOpen ? 'active' : ''}`}>
-          <Link to="/" className="nav-item" onClick={() => setIsMenuOpen(false)}>Home</Link>
-          <Link to="/about" className="nav-item" onClick={() => setIsMenuOpen(false)}>About Us</Link>
-          <Link to="/programs" className="nav-item" onClick={() => setIsMenuOpen(false)}>Programs</Link>
-          <Link to="/get-involved" className="nav-item" onClick={() => setIsMenuOpen(false)}>Get Involved</Link>
-          <Link to="/contact" className="nav-item" onClick={() => setIsMenuOpen(false)}>Contact Us</Link>
-          <Link to="/donate" className="donate-button" onClick={() => setIsMenuOpen(false)}>Donate</Link>
-        </nav>
-      </header>
+    <nav className={`nav-links ${isMenuOpen ? 'active' : ''}`}>
+      <Link to="/" className="nav-item" onClick={() => setIsMenuOpen(false)}>Home</Link>
+      <Link to="/about" className="nav-item" onClick={() => setIsMenuOpen(false)}>About Us</Link>
+      <Link to="/programs" className="nav-item" onClick={() => setIsMenuOpen(false)}>Programs</Link>
+      <Link to="/get-involved" className="nav-item" onClick={() => setIsMenuOpen(false)}>Get Involved</Link>
+      <Link to="/contact" className="nav-item" onClick={() => setIsMenuOpen(false)}>Contact Us</Link>
+      <Link to="/donate" className="donate-button" onClick={() => setIsMenuOpen(false)}>Donate</Link>
+    </nav>
+  </div>
+</header>
+      
     </div>
   );
 }
